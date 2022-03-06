eed5c36581aa9d8ad17848281cb9e75a1b89dfb6
krithika117 <krithikanithyanandam@gmail.com>
2022-03-06 13:50:04 +0530

    query selector addition

--- script.js
+++ script.js
@@ -6,8 +6,29 @@ let weather = {
                 '&units=metric&appid=' +
                 this.apiKey)
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
     },
-    displayWeather: function(data){
+    displayWeather: function (data) {
+        const {
+            name
+        } = data;
+        const {
+            icon,
+            description
+        } = data.weather[0];
+        const {
+            temp,
+            humidity
+        } = data.main;
+        const {
+            speed
+        } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector('.time-zone').innerText = "Weather in " + name;
+        document.querySelector('.desc').innerText = description;
+        document.querySelector('.temp').innerText = temp + '° C';
+        document.querySelector('#h').innerText = humidity;
+        document.querySelector('#w').innerText = speed;
+        document.querySelector('.w-icon').src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
     }
 };
