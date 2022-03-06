38e757e063269be736eec4f71ba0c8e7b0908105
krithika117 <krithikanithyanandam@gmail.com>
2022-03-06 12:38:10 +0530

    openweatherapi

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
+let weather = {
+    apiKey: '740f562df2c7883dad5b3bb99ff9ffe2',
+    fetchWeather: function (city) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' +
+                city +
+                '&units=metric&appid=' +
+                this.apiKey)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+    displayWeather: function(data){
+
+    }
+};
